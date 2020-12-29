2462a0f65bc1712642b66d1f876a28de9d6228c1
nemunioki <nemunoki@example.com>
2020-12-29 16:21:18 +0900

    fist commit

---
+++ node-twitter.js
@@ -0,0 +1,23 @@
+var Twitter = require('twitter');
+require('dotenv').config();
+
+var client = new Twitter({
+	consumer_key: process.env.CONSUMER_KEY,
+	consumer_secret: process.env.CONSUMER_SECRET,
+	access_token_key: process.env.ACCESS_TOKEN,
+	access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+
+/**
+ * Twitterオブジェクトからスクリーンネームを返却します
+ * @param {object} client Twitterオブジェクト
+ * 
+ * @return {string} スクリーンネーム
+ */
+function getUserName(client) {
+	client.get('account/verify_credentials', function(error, tweet, response) {
+		return tweet.screen_name;
+	});
+}
+
+
